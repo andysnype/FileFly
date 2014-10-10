289cc664b376adfe8181a1287251eb99b96c976a
pdpiech <pdpiech@gmail.com>
2014-10-10 18:54:21 -0400

    Begin user interface design
    - Customize ViewPager tab names
    - Correct MainActivity imports and extends
    - Update AndroidManifest.xml with features and permissions
    - Increase minSdkVersion for Android Beam NFC large files compatibility
    - Delete unnecessary files

--- src/com/procom/filefly/MainActivity.java
+++ src/com/procom/filefly/MainActivity.java
@@ -2,24 +2,22 @@ package com.procom.filefly;
 
 import java.util.Locale;
 
-import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 
-public class MainActivity extends Activity implements ActionBar.TabListener {
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
