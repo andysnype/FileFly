2f30aa115e7d5d092a39fd1634d277a2022daae9
pdpiech <pdpiech@gmail.com>
2014-10-12 17:02:44 -0400

    Add fragment XML layouts and fix crash

--- src/com/procom/filefly/MainActivity.java
+++ src/com/procom/filefly/MainActivity.java
@@ -10,12 +10,8 @@ import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
 
 public class MainActivity extends FragmentActivity implements ActionBar.TabListener
 {
@@ -28,7 +24,10 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
      * may be best to switch to a
      * {@link android.support.v13.app.FragmentStatePagerAdapter}.
      */
-    SectionsPagerAdapter mSectionsPagerAdapter;
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private Fragment senderFragment = new SenderFragment();
+    private Fragment receiverFragment = new ReceiverFragment();
+    private Fragment documentListFragment = new DocumentListFragment();
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -131,16 +130,17 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         @Override
         public Fragment getItem(int position)
         {
-//        	switch (position)
-//        	{
-//        	case 0:
-//        	case 1:
-//        	case 2:
-//        	}
-//        	return null;
+        	switch (position)
+        	{
+        	case 0:
+        		return senderFragment;
+        	case 1:
+        		return receiverFragment;
+        	case 2:
+        		return documentListFragment;
+        	}
+        	return null;
             // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
         }
 
         @Override
@@ -164,40 +164,6 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
                     return getString(R.string.title_section3).toUpperCase(l);
             }
             return null;
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment
-    {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber)
-        {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {}
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
         }
     }
 }
--- src/com/procom/filefly/SenderFragment.java
+++ src/com/procom/filefly/SenderFragment.java
@@ -1,8 +1,17 @@
 package com.procom.filefly;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class SenderFragment extends Fragment
 {
-
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		View rootView = inflater.inflate(R.layout.sender_fragment, container, false);
+        return rootView;
+	}
 }
