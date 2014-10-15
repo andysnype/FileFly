e8e1acf2f679272b6d820494d65ee2b39efe069d
pdpiech <pdpiech@gmail.com>
2014-10-15 16:07:13 -0400

    Begin implementation of receive functionality
    - Add code authored by Saurabh Sharma (@saurabhrpi) into ReceiveActivity
    - Add XML authored by Saurabh Sharma (@saurabhrpi) into AndroidManifest
    - Comment out malformed XML authored by by Saurabh Sharma (@saurabhrpi)
    in AndroidManifest
    - Add new XML layout file for ReceiveActivity
    - Delete obsolete file receiver_fragment.xml

--- src/com/procom/filefly/ReceiveActivity.java
+++ src/com/procom/filefly/ReceiveActivity.java
@@ -1,7 +1,14 @@
 package com.procom.filefly;
 
+import java.io.File;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,17 +18,19 @@ import android.view.MenuItem;
  * file into the received subdirectory of the FileFly directory found
  * on the root of the SD Card.
  * 
- * @author Peter Piech
+ * @author Saurabh Sharma, Peter Piech
  *
  */
 public class ReceiveActivity extends Activity
 {
+	private String mParentPath; // A File object containing the path to the transferred files
+    private Intent mIntent; // Incoming Intent
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState)
     {
 		super.onCreate(savedInstanceState);
-        // TODO: create an XML layout and inflate it here
+		setContentView(R.layout.activity_receive);
         // TODO: implement response to ACTION_VIEW intent to fetch transferred file
 		// TODO: add file record to internal SQLite database for use by DocumentListFragment
         // TODO: initiate the MainActivity after above operations complete
@@ -47,5 +56,98 @@ public class ReceiveActivity extends Activity
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+	
+	/**
+	 * <Add Description Here Saurabh>
+	 * 
+	 * @author Saurabh Sharma
+	 * 
+	 */
+	private void handleViewIntent() 
+    {
+        // Get the Intent action
+        mIntent = getIntent();
+        String action = mIntent.getAction();
+        /*
+         * For ACTION_VIEW, the Activity is being asked to display data.
+         * Get the URI.
+         */
+        if (TextUtils.equals(action, Intent.ACTION_VIEW)) {
+            // Get the URI from the Intent
+            Uri beamUri = mIntent.getData();
+            /*
+             * Test for the type of URI, by getting its scheme value
+             */
+            if (TextUtils.equals(beamUri.getScheme(), "file")) {
+                mParentPath = handleFileUri(beamUri);
+            } else if (TextUtils.equals(
+                    beamUri.getScheme(), "content")) {
+                mParentPath = handleContentUri(beamUri);
+            }
+        }
+    }
+    
+	/**
+	 * To get the directory path, get the path part of the URI, which contains all of the URI 
+     * except the file: prefix. Create a File from the path part, then get the parent path of the File:
+	 * 
+	 * @author Saurabh Sharma
+	 * 
+	 */
+    public String handleFileUri(Uri beamUri) 
+    {
+        // Get the path part of the URI
+        String fileName = beamUri.getPath();
+        // Create a File object for this filename
+        File copiedFile = new File(fileName);
+        // Get a string containing the file's parent directory
+        return copiedFile.getParent();
+    }
+    
+    /**
+     * To test the authority of the content URI and retrieve the the path and file name for the 
+     * transferred file
+     * 
+     * @author Saurabh Sharma
+     * 
+     */
+    public String handleContentUri(Uri beamUri) {
+        // Position of the filename in the query Cursor
+        int filenameIndex;
+        // File object for the filename
+        File copiedFile;
+        // The filename stored in MediaStore
+        String fileName;
+        // Test the authority of the URI
+        if (!TextUtils.equals(beamUri.getAuthority(), MediaStore.AUTHORITY)) {
+            /*
+             * As of now not including code to handle content URIs for other content providers
+             */
+        	return null;   
+        // For a MediaStore content URI
+        } else {
+            // Get the column that contains the file name
+            String[] projection = { MediaStore.MediaColumns.DATA };
+            Cursor pathCursor =
+                    getContentResolver().query(beamUri, projection,
+                    null, null, null);
+            // Check for a valid cursor
+            if (pathCursor != null &&
+                    pathCursor.moveToFirst()) {
+                // Get the column index in the Cursor
+                filenameIndex = pathCursor.getColumnIndex(
+                        MediaStore.MediaColumns.DATA);
+                // Get the full file name including path
+                fileName = pathCursor.getString(filenameIndex);
+                // Create a File object for the filename
+                copiedFile = new File(fileName);
+                // Return the parent directory of the file
+                return new File(copiedFile.getParent()).toString();
+             } else {
+                // The query didn't work; return null
+                return null;
+             }
+        }
     }
 }
