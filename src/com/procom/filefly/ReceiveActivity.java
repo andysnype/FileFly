e3a9f976356c45be536194a568119376d24c94b4
Jacob Abramson <jacob@jabramson.com>
2014-10-29 01:26:29 -0400

    Revert "uh"
    This reverts commit 204bdeba0d6b6f2b6c67da0de08ae56ac6ad4ddf, reversing
    changes made to 6982ea959a9c55d0b985b5d114d50fd7b9e12572.

--- src/com/procom/filefly/ReceiveActivity.java
+++ src/com/procom/filefly/ReceiveActivity.java
@@ -126,8 +126,19 @@ public class ReceiveActivity extends FragmentActivity
         return copiedFile.getAbsolutePath();
     }
     
+    /**
+     * To parse out filename's first and last name attributes given
+     * an absolute path to the file
+     * 
+     * @author Jacob Abramson
+     */
+    
     // parses out first and last names and original filename
-    private void grabNameFile(String fn) {
+    private void grabNameFile(String absPath) {
+    	
+    	// parse filename from absolute path
+    	String[] delimSlashes = absPath.split("/");
+    	String fn = delimSlashes[delimSlashes.length - 1];
     	
     	// split string based on underscores
     	String[] result = fn.split("_");
